a5c9ca045fb6f1cbabb79e5c4c99eb09e641228b
Andrew <assliusarenko@gmail.com>
2021-04-21 23:11:49 +0300

    Initial commit

---
+++ src/main/java/com/sliusar/projectzone/ProjectZoneApplication.java
@@ -0,0 +1,13 @@
+package com.sliusar.projectzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectZoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectZoneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sliusar/projectzone/ProjectZoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sliusar.projectzone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectZoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
