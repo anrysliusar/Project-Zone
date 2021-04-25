cd109b7e86804497af514e3547ee92c0ea2df394
Andrew <assliusarenko@gmail.com>
2021-04-25 19:04:30 +0300

    add user service interface

---
+++ src/main/java/com/sliusar/projectzone/services/UserService.java
@@ -0,0 +1,15 @@
+package com.sliusar.projectzone.services;
+
+import com.sliusar.projectzone.models.Project;
+import com.sliusar.projectzone.models.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserService {
+    List<User> getAll();
+    Optional<User> getById(int id);
+    void save(User user);
+    void update(User user, int id);
+    void addRole(User user, int roleId);
+}
