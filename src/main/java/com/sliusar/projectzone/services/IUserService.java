5b9f0051f975bb0f3771b90e8fce20464b5356ec
Andrew <assliusarenko@gmail.com>
2021-05-06 20:25:43 +0300

    add new methods

--- src/main/java/com/sliusar/projectzone/services/IUserService.java
+++ src/main/java/com/sliusar/projectzone/services/IUserService.java
@@ -1,6 +1,8 @@
 package com.sliusar.projectzone.services;
 
 import com.sliusar.projectzone.models.Project;
+import com.sliusar.projectzone.models.Skill;
+import com.sliusar.projectzone.models.Task;
 import com.sliusar.projectzone.models.User;
 
 import java.util.List;
@@ -11,5 +13,9 @@ public interface IUserService {
     Optional<User> getById(int id);
     void save(User user);
     void update(User user, int id);
+    void addSkill(Skill skill, int userId);
+    void addToProject(int projectId, int userId);
+    void assignTask(int taskId, int userId);
+
     void addRole(User user, int roleId);
 }
