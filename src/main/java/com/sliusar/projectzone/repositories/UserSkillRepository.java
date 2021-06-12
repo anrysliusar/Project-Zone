701370d1fbec3e3815cd426d75a6ba8d97379958
Andrew <assliusarenko@gmail.com>
2021-06-12 17:01:37 +0300

    add UserSkillRepository

---
+++ src/main/java/com/sliusar/projectzone/repositories/UserSkillRepository.java
@@ -0,0 +1,7 @@
+package com.sliusar.projectzone.repositories;
+
+import com.sliusar.projectzone.models.UserSkill;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserSkillRepository extends JpaRepository<UserSkill, Integer> {
+}
